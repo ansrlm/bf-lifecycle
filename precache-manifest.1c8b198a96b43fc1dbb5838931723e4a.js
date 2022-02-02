078002adb545d43bf368eb3772f6705b3c48609b
key <ansrlm@daum.net>
2022-02-02 16:47:58 +0900

    Updates

---
+++ precache-manifest.1c8b198a96b43fc1dbb5838931723e4a.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd36a9a73f657edf105c28e2355bfbe",
+    "url": "/bf-lifecycle/index.html"
+  },
+  {
+    "revision": "c9a4de7c40cacb02f938",
+    "url": "/bf-lifecycle/static/js/main.a8030a92.chunk.js"
+  },
+  {
+    "revision": "28c922fdb4c67ad8f3f3",
+    "url": "/bf-lifecycle/static/js/runtime-main.8a3daba3.js"
+  }
+]);
